0e967874edbb661211c9f401d1c8d6a287b38736
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 11:53:37 +0100

    added method

--- calc_mb.py
+++ calc_mb.py
@@ -1,35 +1,25 @@
-from copy import copy
+from dataclasses import dataclass
 from statistics import mean
-from typing import List, NamedTuple
 
 from utils import calc_pixel_brightness
 
-from dataclasses import dataclass, field
 
-@dataclass(frozen=False)
+@dataclass(frozen=True)
 class Pixel:
-    red: List = field(repr=False, default_factory=list)
-    green: List = field(default_factory=list)
-    blue: List = field(default_factory=list)
+    red: float
+    green: float
+    blue: float
 
-pixel = Pixel()
-pixel.red = [2, 3, 4]
-
-
-pixel2 = Pixel()
-print(pixel)
-print(pixel2)
-# print(pixel, pixel2)
+    def get_brightness(self):
+        return calc_pixel_brightness(r=self.red, g=self.green, b=self.blue)
 
 
-#
-# pixel = Pixel(100, 0, 0)
-# pixel2 = Pixel(red=50, green=100)
-# print(pixel)
-# print(pixel2)
-#
-# pixel_brightness = calc_pixel_brightness(pixel.red, pixel.green, pixel.blue)
-# pixel_brightness2 = calc_pixel_brightness(pixel2.red, pixel2.green, pixel2.blue)
-#
-# mean_brightness = mean([pixel_brightness, pixel_brightness2])
-# print(mean_brightness)
+
+pixel = Pixel(100, 0, 0)
+pixel2 = Pixel(red=50, green=100, blue=0)
+
+pixel_brightness = pixel.get_brightness()
+pixel_brightness2 = pixel2.get_brightness()
+
+mean_brightness = mean([pixel_brightness, pixel_brightness2])
+print(mean_brightness)
