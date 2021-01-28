add5b8c021fb451089a31cd8ff173bf533e13fa3
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 10:30:03 +0100

    Packed RGB into tuple

--- calc_mb.py
+++ calc_mb.py
@@ -1,19 +1,14 @@
 from utils import calc_pixel_brightness
 
-red = 100
-red2 = 50
 
-green = 0
-green2 = 100
-
-blue = 0
-blue2 = 0
+pixel = (100, 0, 0)
+pixel2 = (50, 100, 0)
 
 readiness = 0
 readiness2 = 200
 
-pixel_brightness = calc_pixel_brightness(red, green, blue)
-pixel_brightness2 = calc_pixel_brightness(red2, green2, blue2)
+pixel_brightness = calc_pixel_brightness(pixel[0], pixel[1], pixel[2])
+pixel_brightness2 = calc_pixel_brightness(pixel2[0], pixel2[1], pixel2[2])
 
 
 mean_brightness = (pixel_brightness + pixel_brightness2) / 2
