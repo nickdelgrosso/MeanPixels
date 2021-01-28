3e6e679d0907ac177419c946cccaaeb978ad948d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 11:09:23 +0100

    changed to NamedTuple

--- calc_mb.py
+++ calc_mb.py
@@ -2,20 +2,22 @@ from statistics import mean
 
 from utils import calc_pixel_brightness
 
-from collections import namedtuple
+from typing import NamedTuple
 
-Pixel = namedtuple("Pixel","red,green,blue")
+
+class Pixel(NamedTuple):
+    red: float
+    green: float
+    blue: float = 0
+
 
 pixel = Pixel(100, 0, 0)
-pixel2 = Pixel(blue=0, red=50, green=100)
+pixel2 = Pixel(red=50, green=100)
 print(pixel)
 print(pixel2)
 
 pixel_brightness = calc_pixel_brightness(pixel.red, pixel.green, pixel.blue)
 pixel_brightness2 = calc_pixel_brightness(pixel2[0], pixel2[1], pixel2[2])
 
-
 mean_brightness = mean([pixel_brightness, pixel_brightness2])
 print(mean_brightness)
-
-
