7476345c1e04069544f56c755943e452565fc95a
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 10:59:12 +0100

    delete readiness

--- calc_mb.py
+++ calc_mb.py
@@ -6,8 +6,6 @@ from utils import calc_pixel_brightness
 pixel = (100, 0, 0)
 pixel2 = (50, 100, 0)
 
-readiness = 0
-readiness2 = 200
 
 pixel_brightness = calc_pixel_brightness(pixel[0], pixel[1], pixel[2])
 pixel_brightness2 = calc_pixel_brightness(pixel2[0], pixel2[1], pixel2[2])
