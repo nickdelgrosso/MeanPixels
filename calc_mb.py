37326399a40a3b9ac6ddbd8cbd60bf73d5dd1a45
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 10:14:20 +0100

    delete comment

--- calc_mb.py
+++ calc_mb.py
@@ -1,24 +1,24 @@
-red = 100  # Red value
+red = 100
 red2 = 50
 
-green = 0  # Green value
+green = 0
 green2 = 100
 
-blue = 0  # Blue value
+blue = 0
 blue2 = 0
 
-readiness = 0  # Readiness value
+readiness = 0
 readiness2 = 200
+
 
 def get_pixel_brightness(r, g, b):
     return (r + g + b) / 3
 
-# Get pixel brightnesses
+
 pixel_brightness = get_pixel_brightness(red, green, blue)
 pixel_brightness2 = get_pixel_brightness(red2, green2, blue2)
 
 
-# Get mean brightness
 mean_brightness = (pixel_brightness + pixel_brightness2) / 2
 print(mean_brightness)
 
