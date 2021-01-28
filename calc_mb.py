2efa37bdb27188471872fd863083025b3e40ecfc
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 10:20:20 +0100

    new file for function

--- calc_mb.py
+++ calc_mb.py
@@ -1,3 +1,5 @@
+from utils import calc_pixel_brightness
+
 red = 100
 red2 = 50
 
@@ -10,13 +12,8 @@ blue2 = 0
 readiness = 0
 readiness2 = 200
 
-
-def get_pixel_brightness(r, g, b):
-    return (r + g + b) / 3
-
-
-pixel_brightness = get_pixel_brightness(red, green, blue)
-pixel_brightness2 = get_pixel_brightness(red2, green2, blue2)
+pixel_brightness = calc_pixel_brightness(red, green, blue)
+pixel_brightness2 = calc_pixel_brightness(red2, green2, blue2)
 
 
 mean_brightness = (pixel_brightness + pixel_brightness2) / 2
---
+++ utils.py
@@ -0,0 +1,2 @@
+def calc_pixel_brightness(r, g, b):
+    return (r + g + b) / 3
