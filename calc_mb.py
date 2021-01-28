22774c51b1a83d0a993b3715571ec9e696e06575
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 11:06:58 +0100

    added namedtuple

--- calc_mb.py
+++ calc_mb.py
@@ -2,12 +2,16 @@ from statistics import mean
 
 from utils import calc_pixel_brightness
 
+from collections import namedtuple
 
-pixel = (100, 0, 0)
-pixel2 = (50, 100, 0)
+Pixel = namedtuple("Pixel","red,green,blue")
 
+pixel = Pixel(100, 0, 0)
+pixel2 = Pixel(blue=0, red=50, green=100)
+print(pixel)
+print(pixel2)
 
-pixel_brightness = calc_pixel_brightness(pixel[0], pixel[1], pixel[2])
+pixel_brightness = calc_pixel_brightness(pixel.red, pixel.green, pixel.blue)
 pixel_brightness2 = calc_pixel_brightness(pixel2[0], pixel2[1], pixel2[2])
 
 
