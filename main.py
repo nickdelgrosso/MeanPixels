353f12027aa70141bf7683a24a73c86def230069
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 13:56:15 +0100

    show dataclass transformations

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from statistics import mean
 
 from models import Pixel
-
+from dataclasses import asdict, astuple
 
 pixel = Pixel(100, 0, 0)
+print(pixel)
+
+pd = asdict(pixel)
+print(pd)
+
+pt = astuple(pixel)
+print(pt)
+
 pixel2 = Pixel(red=50, green=100, blue=0)
 
 mean_brightness = mean([pixel.brightness, pixel2.brightness])
