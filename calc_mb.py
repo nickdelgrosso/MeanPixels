c42950c0e287d3bae5f3874b9367dbbfa2899e0a
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-28 09:31:55 +0100

    renamed

--- calc_mb.py
+++ calc_mb.py
@@ -1,22 +1,22 @@
-x = 100  # Red value
-x2 = 50
+red = 100  # Red value
+red2 = 50
 
-y = 0  # Green value
-y2 = 100
+green = 0  # Green value
+green2 = 100
 
-z = 0  # Blue value
-z2 = 0
+blue = 0  # Blue value
+blue2 = 0
 
-w = 0  # Readiness value
-w2 = 200
+readiness = 0  # Readiness value
+readiness2 = 200
 
 # Get pixel brightnesses
-result = (x + y + z) / 3
-result2 = (x2 + y2 + z2) / 3
+pixel_brightness = (red + green + blue) / 3
+pixel_brightness2 = (red2 + green2 + blue2) / 3
 
 
 # Get mean brightness
-result3 = (result + result2) / 2
-print(result3)
+mean_brightness = (pixel_brightness + pixel_brightness2) / 2
+print(mean_brightness)
 
 
